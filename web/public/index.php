9ca49154b01b9cc051853f9c1f5b47afbeb1cfa5
Ville Siltala <ville.siltala@geniem.com>
2019-05-26 21:43:55 +0300

    Finalize the index.php for 1 assignment.

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,22 @@
 <?php
 
 include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
+
+// An example of creating an event.
+$event = new App\Event();
+
+// TODO: create a couple of events with some data..
 
 ?><!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
+        <title>My events</title>
     </head>
     <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
+        <h1>My events</h1>
+        <ul>
+            <li>Implement the list..</li>
+        </ul>
     </body>
 </html>
